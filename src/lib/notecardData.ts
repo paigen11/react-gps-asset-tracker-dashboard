0ae12e35ba0bc95a7fa9e6ed48590f627f3e73a6
Paige Niedringhaus <paigen11@gmail.com>
2021-11-15 13:37:26 -0500

    making event fetching more elegant and robust

--- src/lib/notecardData.ts
+++ src/lib/notecardData.ts
@@ -1,6 +1,8 @@
 export async function fetchNotecardData() {
+  let eventArray: object[] = [];
+  // todo make start date dynamic?
   const res = await fetch(
-    `https://api.notefile.net/v1/projects/${process.env.NOTEHUB_PROJECT_ID}/events?pageSize=200`,
+    `https://api.notefile.net/v1/projects/${process.env.NOTEHUB_PROJECT_ID}/events?startDate=1636117939`,
     {
       headers: {
         "Content-Type": "application/json",
@@ -9,13 +11,38 @@ export async function fetchNotecardData() {
     }
   );
   const eventData = await res.json();
-  // console.log(eventData.events.length);
-  const filteredEvents = eventData.events.filter(
-    (event: { file: string; gps_location: object }) =>
-      event.file === "_track.qo"
+  while (eventData.has_more) {
+    const res = await fetch(
+      `https://api.notefile.net/v1/projects/${process.env.NOTEHUB_PROJECT_ID}/events?since=${eventData.through}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-SESSION-TOKEN": `${process.env.NOTEHUB_TOKEN}`,
+        },
+      }
+    );
+    const newEventData = await res.json();
+    eventArray = [...eventArray, ...newEventData.events];
+    if (newEventData.has_more) {
+      eventData.through = newEventData.through;
+    } else {
+      eventData.has_more = false;
+    }
+    console.log(newEventData);
+  }
+  const filteredEvents = eventArray.filter(
+    (event: { file: string }) => event.file === "_track.qo"
   );
   /* todo use notehub api docs to fetch more events until `has_more` shows false
-  use `through` to grab last unique id, pop it into the req url as `since` or just arbitrarily pull a big number with pageSize
+    use `through` to grab last unique id, pop it into the req url as `since` or just arbitrarily pull a big number with pageSize
     https://dev.blues.io/reference/notehub-api/event-api/ */
+
+  // ray's notes
+  // would assume that the algorithm would be:
+  // Do an initial request to GetEventsByProject with a startDate indicating how far back you want to start
+  // Go into a loop While (response.has_more) {
+  //         do another request using "request.since = response.through"
+  // }
+  console.log("count of filtered events", filteredEvents);
   return filteredEvents;
 }
