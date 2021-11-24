8de56e631d0d2cc2da05c3a939b821d9c3778b39
Paige Niedringhaus <paigen11@gmail.com>
2021-11-24 13:42:54 -0500

    styling the chart a little

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -6,8 +6,10 @@ import {
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
+  Label,
 } from "recharts";
 import CustomTooltip from "./CustomTooltip";
+import "../../styles/Chart.module.scss";
 
 type tempDataProps = {
   shortenedDate: string;
@@ -16,10 +18,8 @@ type tempDataProps = {
 }[];
 
 const Chart = ({ tempData }: { tempData: tempDataProps }) => {
-  // todo add scss file styling
-
   return (
-    <ResponsiveContainer width="100%" height={200}>
+    <ResponsiveContainer width={1000} height={400}>
       <AreaChart
         width={500}
         height={400}
@@ -32,8 +32,12 @@ const Chart = ({ tempData }: { tempData: tempDataProps }) => {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="shortenedDate" label="Date" />
-        <YAxis dataKey="temp" label="Temperature (F)" />
+        <XAxis dataKey="shortenedDate">
+          <Label value="Date" position="bottom" />
+        </XAxis>
+        <YAxis dataKey="temp">
+          <Label value="Temperature" angle={-90} position="insideLeft" />
+        </YAxis>
         <Tooltip content={<CustomTooltip payload={tempData} />} />
         <Area type="monotone" dataKey="temp" stroke="#8884d8" fill="#8884d8" />
       </AreaChart>
