916acd9573c800517023a64694da881c5517ed00
Paige Niedringhaus <paigen11@gmail.com>
2021-12-01 10:16:52 -0500

    removing api routes

--- pages/api/post/notecard.ts
+++
@@ -1,9 +0,0 @@
-// todo possibly delete this no longer needed route
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function receiveNotecardData(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.status(200).json(req.body);
-}
